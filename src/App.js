a372da23d843eadfb71dfe675a4e7683aae9cb1a
sashida <sashida@primagest.co.jp>
2020-09-24 02:31:21 +0900

    p.361

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Message } from "./Message";
 import { List } from "./List";
+import { ExternalCounter } from "./ExternalCounter";
 
 export default class App extends Component {
   constructor(props) {
@@ -19,13 +20,16 @@ export default class App extends Component {
 
     return <div className="container text-center">
       <div className="row p-2">
-        <div className="col-6">
+        <div className="col-4">
           <Message message={ `Counter: ${this.state.counter}` }
             callback={ this.incrementCounter }
             text="Increment Counter" />
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <List />
+        </div>
+        <div className="col-4">
+          <ExternalCounter />
         </div>
       </div>
     </div>
