7fcaf597fe1cf240540625e6862b020e6a268510
sashida <sashida@primagest.co.jp>
2020-09-24 02:37:18 +0900

    p.364

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import { List } from "./List";
 import { ExternalCounter } from "./ExternalCounter";
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      showMessage: true
     };
   }
 
   incrementCounter= () => {
     this.setState({ counter: this.state.counter + 1 });
+  }
+
+  handleChange = () => {
+    this.setState({ showMessage: !this.state.showMessage });
   }
 
   render() {
@@ -21,9 +27,16 @@ export default class App extends Component {
     return <div className="container text-center">
       <div className="row p-2">
         <div className="col-4">
-          <Message message={ `Counter: ${this.state.counter}` }
-            callback={ this.incrementCounter }
-            text="Increment Counter" />
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" checked={ this.state.showMessage }
+              onChange={ this.handleChange } />
+            <label className="form-check-label">Show</label>
+          </div>
+          { this.state.showMessage &&
+            <Message message={ `Counter: ${this.state.counter}` }
+              callback={ this.incrementCounter }
+              text="Increment Counter" />
+          }
         </div>
         <div className="col-4">
           <List />
