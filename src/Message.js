d3f92fae918e921a746abb6de78be45005bab9cc
sashida <sashida@primagest.co.jp>
2020-09-25 08:49:38 +0900

    p.367

--- src/Message.js
+++ src/Message.js
@@ -41,4 +41,8 @@ export class Message extends Component {
     componentDidUpdate() {
         console.log("componentDidUpdate Message Component");
     }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount Message Component");
+    }
 }
