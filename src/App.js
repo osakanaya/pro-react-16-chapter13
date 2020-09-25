7356109ec6394fa67db61e48dbc3959702506210
sashida <sashida@primagest.co.jp>
2020-09-25 09:03:23 +0900

    p.374

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import { Message } from "./Message";
-import { List } from "./List";
-import { ExternalCounter } from "./ExternalCounter";
-import { HooksMessage } from "./HooksMessage";
+//import { List } from "./List";
+//import { ExternalCounter } from "./ExternalCounter";
+//import { HooksMessage } from "./HooksMessage";
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      showMessage: true
+      counterLeft: 0,
+      counterRight: 0
     };
   }
 
-  incrementCounter= () => {
-    this.setState({ counter: this.state.counter + 1 });
+  incrementCounter= (counter) => {
+    if (counter === "left") {
+      this.setState({ counterLeft: this.state.counterLeft + 1 });
+    } else {
+      this.setState({ counterRight: this.state.counterRight + 1 });
+    }
   }
 
   handleChange = () => {
@@ -27,28 +31,15 @@ export default class App extends Component {
 
     return <div className="container text-center">
       <div className="row p-2">
-        <div className="col-4">
-          <div className="form-check">
-            <input type="checkbox" className="form-check-input" checked={ this.state.showMessage }
-              onChange={ this.handleChange } />
-            <label className="form-check-label">Show</label>
-          </div>
-          { this.state.showMessage &&
-            <div>
-              <Message message={ `Counter: ${this.state.counter}` }
-                callback={ this.incrementCounter }
-                text="Increment Counter" />
-              <HooksMessage message={ `Counter: ${this.state.counter}` }
-                callback={ this.incrementCounter }
-                text="Increment Counter" />
-            </div>
-          }
+        <div className="col-6">
+          <Message message={ `Left: ${this.state.counterLeft}` }
+                  callback={ () => this.incrementCounter("left") }
+                  text="Increment Left Counter" />
         </div>
-        <div className="col-4">
-          <List />
-        </div>
-        <div className="col-4">
-          <ExternalCounter />
+        <div className="col-6">
+          <Message message={ `Right: ${this.state.counterRight}` }
+                    callback={ () => this.incrementCounter("right") }
+                    text="Increment Right Counter" />
         </div>
       </div>
     </div>
--- src/Message.js
+++ src/Message.js
@@ -34,6 +34,7 @@ export class Message extends Component {
         )
     }
 
+    /*
     componentDidMount() {
         console.log("componentDidMount Message Component");
     }
@@ -44,5 +45,18 @@ export class Message extends Component {
 
     componentWillUnmount() {
         console.log("componentWillUnmount Message Component");
+    }
+    */
+
+    shouldComponentUpdate(newProps, newState) {
+        let change = newProps.message !== this.props.message;
+
+        if (change) {
+            console.log(`shouldComponentUpdate ${this.props.text}: Update Allowed`);
+        } else {
+            console.log(`shouldComponentUpdate ${this.props.text}: Update Prevented`);
+        }
+
+        return change;
     }
 }
